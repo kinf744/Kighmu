be6a24d5dbfa8479a1396d8ea09cd1f23f7e4173
kinf744 <adrienyuoi@gmail.com>
2025-08-30 08:10:38 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 81 });
+
+wss.on('connection', (ws) => {
+  console.log('Un client WebSocket est connecté');
+
+  ws.on('message', (message) => {
+    console.log('Message reçu: %s', message);
+    // Renvoie en écho le message reçu
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client déconnecté');
+  });
+
+  // Message de bienvenue à la connexion
+  ws.send('Bienvenue sur le serveur WebSocket!');
+});
+
+console.log('Serveur WebSocket démarré et écoute sur le port 81');
+          
