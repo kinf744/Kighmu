6b7c0620b2942c3c2abefb77d5b0834499f23d54
kinf744 <adrienyuoi@gmail.com>
2025-09-03 18:16:48 +0100

    Update install_kighmu.sh

--- install_kighmu.sh
+++ install_kighmu.sh
@@ -71,12 +71,60 @@ echo "=============================================="
 echo " 🚀 Installation des paquets essentiels..."
 echo "=============================================="
 
-# Mise à jour et upgrade du système
 apt update -y && apt upgrade -y
 
-for pkg in sudo bsdmainutils zip unzip ufw curl python3 python3-pip openssl screen cron iptables lsof pv boxes nano at mlocate gawk grep bc jq npm nodejs socat netcat netcat-traditional net-tools cowsay figlet lolcat dnsutils wget psmisc nginx dropbear python3-setuptools wireguard-tools qrencode gcc make perl systemd tcpdump iproute2 tmux git build-essential libssl-dev software-properties-common; do
-  apt install -y "$pkg"
-done
+apt install -y sudo
+apt install -y bsdmainutils
+apt install -y zip
+apt install -y unzip
+apt install -y ufw
+apt install -y curl
+apt install -y python3
+apt install -y python3-pip
+apt install -y openssl
+apt install -y screen
+apt install -y cron
+apt install -y iptables
+apt install -y lsof
+apt install -y pv
+apt install -y boxes
+apt install -y nano
+apt install -y at
+apt install -y mlocate
+apt install -y gawk
+apt install -y grep
+apt install -y bc
+apt install -y jq
+apt install -y npm
+apt install -y nodejs
+apt install -y socat
+apt install -y netcat
+apt install -y netcat-traditional
+apt install -y net-tools
+apt install -y cowsay
+apt install -y figlet
+apt install -y lolcat
+apt install -y dnsutils
+apt install -y wget
+apt install -y psmisc
+apt install -y nginx
+apt install -y dropbear
+apt install -y python3-setuptools
+apt install -y wireguard-tools
+apt install -y qrencode
+apt install -y gcc
+apt install -y make
+apt install -y perl
+apt install -y systemd
+apt install -y tcpdump
+apt install -y iptables
+apt install -y iproute2
+apt install -y net-tools
+apt install -y tmux
+apt install -y git
+apt install -y build-essential
+apt install -y libssl-dev
+apt install -y software-properties-common
 
 apt autoremove -y
 apt clean
