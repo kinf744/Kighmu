c085c4c6df267cae3bdcc1ab93d2a36550023267
kinf744 <adrienyuoi@gmail.com>
2025-09-04 22:59:27 +0100

    Update install_kighmu.sh

--- install_kighmu.sh
+++ install_kighmu.sh
@@ -160,6 +160,8 @@ FILES=(
   "setup_ssh_config.sh"
   "create_ssh_user.sh"
   "menu4_2.sh"
+  "socks_python.service"
+  "slowdns.service"
 )
 
 BASE_URL="https://raw.githubusercontent.com/kinf744/Kighmu/main"
