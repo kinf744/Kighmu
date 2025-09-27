43b40bb6a9f8655f5c043a83da375cfa1cfd4a66
kinf744 <adrienyuoi@gmail.com>
2025-09-27 02:00:58 +0100

    Update menu5.sh

--- menu5.sh
+++ menu5.sh
@@ -358,6 +358,7 @@ while true; do
     echo -e "${GREEN}${BOLD}[08]${RESET} ${YELLOW}proxy ws${RESET}"
     echo -e "${GREEN}${BOLD}[09]${RESET} ${YELLOW}Hysteria${RESET}"
     echo -e "${GREEN}${BOLD}[00]${RESET} ${YELLOW}Quitter${RESET}"
+    echo -e "${CYAN}+------------------------------------------------------+${RESET}"
     echo -ne "${BOLD}${YELLOW}👉 Choisissez un mode : ${RESET}"
     read choix
     case $choix in
