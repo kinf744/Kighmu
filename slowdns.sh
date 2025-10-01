0188451e425fc3bf22443ee3b8ac9adce5c8ae1d
kinf744 <adrienyuoi@gmail.com>
2025-10-01 06:32:57 +0100

    Update slowdns.sh

--- slowdns.sh
+++ slowdns.sh
@@ -163,7 +163,7 @@ main() {
   fi
   log "Interface réseau détectée : $interface"
 
-  MTU_VALUE=$(($(cat /sys/class/net/$interface/mtu) - 1500))
+  MTU_VALUE=$(($(cat /sys/class/net/$interface/mtu) - 100))
   log "Réglage MTU sur interface $interface à $MTU_VALUE..."
   ip link set dev "$interface" mtu "$MTU_VALUE"
 
