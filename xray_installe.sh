64bc4f3cf18a46fde4005c0a657a914325dc5d4e
kinf744 <adrienyuoi@gmail.com>
2025-10-21 23:33:50 +0100

    Update xray_installe.sh

--- xray_installe.sh
+++ xray_installe.sh
@@ -116,11 +116,7 @@ cat > /etc/xray/config.json << EOF
           }],
           "minVersion": "1.2",
           "maxVersion": "1.3",
-          "cipherSuites": [
-            "TLS_AES_128_GCM_SHA256",
-            "TLS_AES_256_GCM_SHA384",
-            "TLS_CHACHA20_POLY1305_SHA256"
-          ]
+          "cipherSuites": "TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
         },
         "wsSettings": {
           "path": "/vmess-tls",
