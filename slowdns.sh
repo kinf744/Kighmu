1a0055ca6ea1d22fde7530005c5ea74c4d4c56a1
kinf744 <adrienyuoi@gmail.com>
2025-12-10 14:53:05 +0100

    Update slowdns.sh

--- slowdns.sh
+++ slowdns.sh
@@ -282,7 +282,7 @@ EOF
     echo "+--------------------------------------------+"
     echo ""
     echo "Clé publique : $PUB_KEY"
-    echo "NameServer  : $NS"
+    echo "NameServer  : $NAMESERVER"
     echo ""
     echo "MTU du tunnel : 1400"
     log "Installation et configuration SlowDNS terminées."
