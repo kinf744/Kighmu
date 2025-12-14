b4c3242fd1a6f56108586e53f6952feec5a3d52e
kinf744 <adrienyuoi@gmail.com>
2025-12-14 17:31:20 +0100

    Update menu_5.sh

--- menu_5.sh
+++ menu_5.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Fichier stockage utilisateurs
+mkdir -p /etc/v2ray
+touch /etc/v2ray/utilisateurs.json
+chmod 600 /etc/v2ray/utilisateurs.json
+
 USER_DB="/etc/v2ray/utilisateurs.json"
 
 # Couleurs ANSI pour mise en forme
