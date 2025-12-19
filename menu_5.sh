9d2887981eb0be7d043a8174f1f5456498767bbd
kinf744 <adrienyuoi@gmail.com>
2025-12-19 13:38:17 +0100

    Update menu_5.sh

--- menu_5.sh
+++ menu_5.sh
@@ -238,7 +238,7 @@ installer_v2ray() {
       "port": 5401,
       "protocol": "dokodemo-door",
       "settings": {
-        "address": "127.0.0.1",
+        "address": "0.0.0.0",
         "port": 22,
         "network": "tcp"
       },
