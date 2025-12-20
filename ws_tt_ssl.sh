cdb1d1c997484b00bf35f007b2c28597e0d43f63
kinf744 <adrienyuoi@gmail.com>
2025-12-20 03:23:39 +0100

    Update ws_tt_ssl.sh

--- ws_tt_ssl.sh
+++ ws_tt_ssl.sh
@@ -183,15 +183,22 @@ cat > /etc/nginx/conf.d/kighmu-ws.conf <<EOF
 server {
     listen 80;
     server_name $DOMAIN;
+
     location /ws-dropbear {
         proxy_pass http://127.0.0.1:2095;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$host;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_read_timeout 86400;
     }
-    location / { return 444; }
+
+    # Refuser tout autre trafic HTTP non WS
+    location / {
+        return 444;
+    }
 }
 
 server {
