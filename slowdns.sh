646e042d47ea3fdb09a62ea81785250821077c1a
kinf744 <adrienyuoi@gmail.com>
2025-12-24 19:09:40 +0100

    Update slowdns.sh

--- slowdns.sh
+++ slowdns.sh
@@ -404,7 +404,7 @@ EOF
     echo "NameServer  : $NS"
     echo "Backend     : $BACKEND"
     echo "Mode        : $MODE"
-    REAL_MTU=$(get_mtu "$INTERFACE")
+    REAL_MTU=$(get_mtu "$interface")
 
     echo "MTU configuré : $SLOWDNS_MTU"
     echo "MTU réel appliqué sur $interface : $REAL_MTU"
