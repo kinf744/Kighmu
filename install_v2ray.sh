dfd6b8ffd7c0f5c84aa0de8ea87c1f3112788c08
kinf744 <adrienyuoi@gmail.com>
2025-12-26 12:47:04 +0100

    Update install_v2ray.sh

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -39,6 +39,16 @@ cat <<EOF | sudo tee /etc/v2ray/config-v2only.json >/dev/null
   "inbounds": [
     {
       "port": 5401,
+      "protocol": "dokodemo-door",
+      "settings": {
+        "address": "0.0.0.0",
+        "port": 22,
+        "network": "tcp"
+      },
+      "tag": "ssh"
+    },
+    {
+      "port": 5401,
       "protocol": "vless",
       "settings": {
         "clients": [
