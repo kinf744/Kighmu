c4377e204d963b0b878c9d1a3a4cc3919c108d09
kinf744 <adrienyuoi@gmail.com>
2025-12-30 14:59:33 +0100

    Update xray_installe.sh

--- xray_installe.sh
+++ xray_installe.sh
@@ -124,16 +124,6 @@ cat > /etc/xray/config.json << EOF
   "inbounds": [
     {
       "port": 8443,
-      "protocol": "dokodemo-door",
-      "settings": {
-        "address": "0.0.0.0",
-        "port": 22,
-        "network": "tcp"
-      },
-      "tag": "ssh"
-    },
-    {
-      "port": 8443,
       "protocol": "vmess",
       "settings": {
         "clients": [{"id": "$uuid1", "alterId": 0}]
