aeed7ad70a5c35a43301e3171f11c0a337634901
kinf744 <adrienyuoi@gmail.com>
2026-01-18 17:07:18 +0100

    Update menu_6.sh

--- menu_6.sh
+++ menu_6.sh
@@ -169,7 +169,7 @@ create_config() {
   case "$proto" in
     vmess)
       path_ws_tls="/vmess"
-      path_ws_ntls="/vmess"
+      path_ws_ntls="/vmess-ntls"
 
       # users.json
       jq --arg id "$uuid" --arg name "$name" --arg tag "$tag" --arg exp "$exp_date_iso" --argjson lim "$limit" \
@@ -196,7 +196,7 @@ create_config() {
 
     vless)
       path_ws_tls="/vless"
-      path_ws_ntls="/vless"
+      path_ws_ntls="/vless-ntls"
 
       jq --arg id "$uuid" --arg name "$name" --arg tag "$tag" --arg exp "$exp_date_iso" --argjson lim "$limit" \
         '.vless += [{
@@ -221,7 +221,7 @@ create_config() {
 
     trojan)
       path_ws_tls="/trojan-ws"
-      path_ws_ntls="/trojan-ws"
+      path_ws_ntls="/trojan-ntls"
 
       jq --arg pw "$uuid" --arg name "$name" --arg tag "$tag" --arg exp "$exp_date_iso" --argjson lim "$limit" \
         '.trojan += [{
