a4b7d14d211d61b213e0cf8ed9ae21c4f7ba5dec
kinf744 <adrienyuoi@gmail.com>
2026-02-02 01:02:12 +0100

    Update zivpn.sh

--- zivpn.sh
+++ zivpn.sh
@@ -105,7 +105,7 @@ install_zivpn() {
   cat > "$ZIVPN_CONFIG" << 'EOF'
 {
   "listen": ":5667",
-  "exclude_port": [53,5300,4466],
+  "exclude_port": [53,5300,4466,20000],
   "cert": "/etc/zivpn/zivpn.crt",
   "key": "/etc/zivpn/zivpn.key",
   "obfs": "zivpn",
