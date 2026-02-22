9fcf0e6807b34e5e214b36583f9b2c656a669ec0
kinf744 <adrienyuoi@gmail.com>
2026-02-22 20:29:27 +0100

    Update menu5.sh

--- menu5.sh
+++ menu5.sh
@@ -350,7 +350,7 @@ Wants=network.target
 
 [Service]
 Type=simple
-ExecStart=$BIN_DST -listen 444 -target-host 127.0.0.1 -target-port 109
+ExecStart=$BIN_DST -listen 444 -target-host 127.0.0.1 -target-port 22
 Restart=always
 RestartSec=2
 LimitNOFILE=1048576
@@ -569,7 +569,7 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=$BIN_DST -listen 80 -target-host 127.0.0.1 -target-port 109
+ExecStart=$BIN_DST -listen 80 -target-host 127.0.0.1 -target-port 22
 Restart=always
 RestartSec=2
 User=root
